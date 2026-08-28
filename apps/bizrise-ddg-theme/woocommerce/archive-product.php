0a268d23644a0591caa4a0fa3dd16bec90382d3a
Phuong Nguyen <duyphuongcutypn@gmail.com>
2026-08-28 08:21:18 +0700

    chore(theme): bust product mockup 2.2.4 cache

--- apps/bizrise-ddg-theme/woocommerce/archive-product.php
+++ apps/bizrise-ddg-theme/woocommerce/archive-product.php
@@ -10,7 +10,7 @@ wp_enqueue_style(
     'bizrise-ddg-product-mockup',
     get_template_directory_uri() . '/assets/css/product-mockup.css',
     ['bizrise-ddg-theme213'],
-    '2.2.3'
+    '2.2.4'
 );
 
 get_header();
